4d4bd110330f0e2260ebcd84815ec7c15d7b55d4
Aaiyub <shaikhaaiyub124@gmail.com>
2026-07-18 21:32:08 +0530

    API created

--- app/shorten/page.js
+++ app/shorten/page.js
@@ -7,6 +7,36 @@ const Shorten = () => {
     const [shorturl, setshorturl] = useState("")
     const [generated, setGenerated] = useState("")
 
+    const generate = () => {
+        const myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+
+        const raw = JSON.stringify({
+            "url": url,
+            "shorturl": shorturl
+        });
+
+        const requestOptions = {
+            method: "POST",
+            headers: myHeaders,
+            body: raw,
+            redirect: "follow"
+        };
+
+        fetch("/api/generate", requestOptions)
+            .then((response) => response.json())
+            .then((result) => {
+                setGenerated(`${process.env.NEXT_PUBLIC_HOST}/${shorturl}`)
+                seturl("")   
+                setshorturl("")
+                console.log(result)
+                alert(result.message)
+            
+            })
+            .catch((error) => console.error(error));
+    }
+
+
     return (
         <div className='mx-auto max-w-lg bg-purple-100 my-16 p-8 rounded-lg flex flex-col gap-4'>
             <h1 className='font-bold text-2xl'>Generate your short URLs</h1>
